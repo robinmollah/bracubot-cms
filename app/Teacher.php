b72d2ad093fe98aed53fd67669e0ed098aba4d71
robinmollah <robinsajin@gmail.com>
2020-08-27 22:06:14 +0600

    Show teachers in table

--- app/Teacher.php
+++ app/Teacher.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     protected $table = 'teachers';
+    protected $primaryKey = 'initial';
+    protected $keyType = 'string';
 }
--- resources/views/teachers/index.blade.php
+++ resources/views/teachers/index.blade.php
@@ -3,5 +3,29 @@
 @section('title', 'View teachers')
 
 @section('content')
-    <p>This is my teachers content {{ $teachers }}</p>
+    <p>All teachers list</p>
+    <br/>
+    <br/>
+    <table>
+        <thead>
+        <tr>
+            <td>Initial</td>
+            <td>Name</td>
+            <td>Department</td>
+            <td>Operations</td>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($teachers as $key => $teacher)
+            <tr>
+                <td>{{$teacher->initial}}</td>
+                <td>{{$teacher->name}}</td>
+                <td>{{$teacher->department}}</td>
+                <td>
+                    <a href="{{ URL::to('teachers/' . $teacher->initial) }}">Details</a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
